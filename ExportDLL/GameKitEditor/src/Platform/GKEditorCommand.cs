73ad690a7442928e14558802a1c3698a6be91313
chenxiaolei <chenxiaolei@shandagames.com>
2019-04-17 17:29:47 +0800

    [GKEditorCommand] 1. 新增Unity 控制台接口;

---
+++ ExportDLL/GameKitEditor/src/Platform/GKEditorCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GKPlatform
+{
+    public class GKEditorCommand
+    {
+        public static void ProcessCommand(string command, string argument)
+        {
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(command);
+            info.Arguments = argument;
+            info.CreateNoWindow = false;
+            info.ErrorDialog = true;
+            info.UseShellExecute = true;
+
+            if (info.UseShellExecute)
+            {
+                info.RedirectStandardOutput = false;
+                info.RedirectStandardError = false;
+                info.RedirectStandardInput = false;
+            }
+            else
+            {
+                info.RedirectStandardOutput = true;
+                info.RedirectStandardError = true;
+                info.RedirectStandardInput = true;
+                info.StandardOutputEncoding = System.Text.UTF8Encoding.UTF8;
+                info.StandardErrorEncoding = System.Text.UTF8Encoding.UTF8;
+            }
+
+            System.Diagnostics.Process process = System.Diagnostics.Process.Start(info);
+
+            if (!info.UseShellExecute)
+            {
+                UnityEngine.Debug.Log(process.StandardOutput);
+                UnityEngine.Debug.Log(process.StandardError);
+            }
+
+            process.WaitForExit();
+            process.Close();
+        }
+
+    }
+}
